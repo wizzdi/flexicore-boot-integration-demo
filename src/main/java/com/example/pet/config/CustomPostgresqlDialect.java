62d40c8a07027e94b5618b507c1bcf2b09f6c101
Asaf Ben Natan <asaf@wizzdi.com>
2021-05-21 02:13:51 +0300

    3.0.0 - dynamic invokers

--- src/main/java/com/example/pet/config/CustomPostgresqlDialect.java
+++ src/main/java/com/example/pet/config/CustomPostgresqlDialect.java
@@ -1,6 +1,8 @@
 package com.example.pet.config;
 
 import org.hibernate.dialect.PostgreSQL92Dialect;
+import org.hibernate.type.WrappedMaterializedBlobType;
+import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
@@ -11,6 +13,9 @@ public class CustomPostgresqlDialect extends PostgreSQL92Dialect {
 
 	@Override
 	public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
+		if (Types.BLOB == sqlTypeDescriptor.getSqlType()) {
+			return BinaryTypeDescriptor.INSTANCE;
+		}
 		if (Types.CLOB == sqlTypeDescriptor.getSqlType()) {
 			return LongVarcharTypeDescriptor.INSTANCE;
 		}
